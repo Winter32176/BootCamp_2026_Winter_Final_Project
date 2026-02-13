7c4906f0103db471a6d13a962c9862c27c3d9807
Robert <robert32176@outlook.com>
2026-02-13 12:40:35 +0200

    init

---
+++ src/main/java/com/example/caplock_booking/CaplockBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.caplock_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaplockBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaplockBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caplock_booking/CaplockBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caplock_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaplockBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
